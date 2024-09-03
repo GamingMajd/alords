34994b2862f10fa63ba0153c1f856b648add2a16
GamingMajd <93816991+GamingMajd@users.noreply.github.com>
2024-09-03 23:16:17 +0300

    Add files via upload

---
+++ Quizzes/Quiz3/script.js
@@ -0,0 +1,37 @@
+let saveFile = () => {
+    // Get the data from each element on the form.
+    const name = document.getElementById("name");
+    const A1 = document.getElementById("A1");
+    const A2 = document.getElementById("A2");
+    const A3 = document.getElementById("A3");
+    const A4 = document.getElementById("A4");
+    const A5 = document.getElementById("A5");
+    const A6 = document.getElementById("A6");
+    const A7 = document.getElementById("A7");
+    const A8 = document.getElementById("A8");
+    const A9 = document.getElementById("A9");
+    const A10 = document.getElementById("A10");
+    const A11 = document.getElementById("A11");
+    const A12 = document.getElementById("A12");
+    const A13 = document.getElementById("A13");
+    const A14 = document.getElementById("A14");
+    const A15 = document.getElementById("A15");
+
+    // This variable stores all the data.
+    let data = " \r " + "Name: " + name.value + " \r\n " + "Answer 1: " + A1.value + " \r\n " + "Answer 2: " + A2.value + " \r\n " + "Answer 3: " + A3.value + " \r\n " + "Answer 4: " + A4.value + " \r\n " + "Answer 5: " + A5.value + " \r\n " + "Answer 6: " + A6.value + " \r\n " + "Answer 7: " + A7.value + " \r\n " + "Answer 8: " + A8.value + " \r\n " + "Answer 9: " + A9.value + " \r\n " + "Answer 10: " + A10.value + " \r\n " + "Answer 11: " + A11.value + " \r\n " + "Answer 12: " + A12.value + " \r\n " + "Answer 13: " + A13.value + " \r\n " + "Answer 14: " + A14.value + " \r\n " + "Answer 15: " + A15.value;
+    console.log(data); //printing form data into the console
+    // Convert the text to BLOB.
+    const textToBLOB = new Blob([data], { type: "text/plain" });
+    let newLink = document.createElement("a");
+    newLink.download = name.value+".txt";
+
+    if (window.webkitURL != null) {
+        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    } else {
+        newLink.href = window.URL.createObjectURL(textToBLOB);
+        newLink.style.display = "none";
+        document.body.appendChild(newLink);
+    }
+
+    newLink.click();
+};
